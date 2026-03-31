79dd9cb7e90d390c0041e112df5c2b8dfb6a3515
HLMC <hl2278535805@outlook.com>
2026-03-31 18:51:43 +0800

    fix ksu permissions

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@ print_modname() {
 
 set_permissions() {
 	set_perm_recursive $MODPATH 0 0 0755 0644
-	set_perm_recursive $MODPATH/system/system_ext/etc 0 0 0755 0644
+	set_perm_recursive $MODPATH/odm/etc 0 0 0755 0644
 	set_perm_recursive $MODPATH/system/vendor/etc 0 0 0755 0644 u:object_r:vendor_configs_file:s0
 }
 
