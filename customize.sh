6167ef78381ad7d1d8a0bbca54ff9c4d7f558280
HLMC <hl2278535805@outlook.com>
2026-07-16 15:35:58 +0800

    refactor: replace so

--- customize.sh
+++ customize.sh
@@ -12,5 +12,20 @@ set_permissions() {
 	set_perm_recursive $MODPATH/system/vendor/etc 0 0 0755 0644 u:object_r:vendor_configs_file:s0
 }
 
+replace_so() {
+  SO_FILES="libaudioclientimpl.so libaudioflingerimpl.so libaudiopolicyserviceimpl.so libmiaudiopolicymanager.so"
+  SEARCH_PATHS="system/lib64 system/system_ext/lib64 system/vendor/lib64 odm/vendor/lib64"
+  for path in $SEARCH_PATHS; do
+    for so in $SO_FILES; do
+      if [ -f "/$path/$so" ]; then
+        mkdir -p "$MODPATH/$path"
+        touch "$MODPATH/$path/$so"
+        ui_print "- Replacing: /$path/$so"
+      fi
+    done
+  done
+}
+
 print_modname
+replace_so
 set_permissions
