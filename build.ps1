7fad64a54bc286380feb7f330e887b1b546593eb
HLMC <hl2278535805@outlook.com>
2026-08-07 00:23:16 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -10,7 +10,7 @@ $zipName = "${name}.zip"
 Write-Host "Building Rust binary..."
 Push-Location $rustDir
 try {
-    cargo ndk -t arm64-v8a build --release
+    cargo ndk -t arm64-v8a build --release --features cirrus
     if ($LASTEXITCODE -ne 0) { throw "cargo build failed" }
 } finally {
     Pop-Location
